9e146d60ae96444f8f40d900db08820f95d56990
Golam Kibria <golam15-14592@diu.edu.bd>
2022-04-22 22:29:48 +0600

    added basic layouts and color

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Gossip</title>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="left_panel">
+
+        </div>
+        <div id="right_panel">
+            <div id="header">Gossip</div>
+            <div id="container">
+                <div id="inner_left_panel">
+
+                </div>
+                <div id="inner_right_panel">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
