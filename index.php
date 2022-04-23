417f190afb749dd630283bf43f48b454be062555
Golam Kibria <golam15-14592@diu.edu.bd>
2022-04-23 10:45:48 +0600

    Completed the UI designs

--- index.php
+++ index.php
@@ -10,19 +10,39 @@
 <body>
     <div id="wrapper">
         <div id="left_panel">
+            <div>
+                <img id="profile_image" src="./ui/images/user3.jpg" alt="">
+                <br>
+                Kelly Hartmann
+                <br>
+                <span>kellyhartmann@gmail.com</span>
 
+                <br>
+                <br>
+                <br>
+
+                <div>
+                    <label id="label_chat" for="radio_chat">Chat <img src="./ui/icons/chat.png" alt=""></label>
+                    <label id="label_contacts" for="radio_contacts">Contacts <img src="./ui/icons/contacts.png" alt=""></label>
+                    <label id="label_settings" for="radio_settings">Settings <img src="./ui/icons/settings.png" alt=""></label>
+                </div>
+            </div>
         </div>
         <div id="right_panel">
             <div id="header">Gossip</div>
             <div id="container">
                 <div id="inner_left_panel">
-
                 </div>
-                <div id="inner_right_panel">
 
+                <input type="radio" id="radio_chat" name="nav">
+                <input type="radio" id="radio_contacts" name="nav">
+                <input type="radio" id="radio_settings" name="nav">
+
+                <div id="inner_right_panel">
                 </div>
             </div>
         </div>
     </div>
+    <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
