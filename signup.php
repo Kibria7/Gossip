be98d2ff67607f82adac624ff0cb534fbcff6d26
Golam Kibria <golam15-14592@diu.edu.bd>
2022-04-25 02:52:33 +0600

    Finished the html and css for the sign up page

---
+++ signup.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/signup.css">
+    <title>Gossip</title>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            Gossip
+            <div>Login</div>
+        </div>
+        <form action="">
+            <input type="text" name="username" placeholder="Username"><br>
+            <div>
+                <span class="gender">Gender:</span><br>
+                <input type="radio" name="gender"> Male<br>
+                <input type="radio" name="gender"> Female<br>
+            </div>
+            <input type="Password" name="password" placeholder="Password"><br>
+            <input type="password" name="password2" placeholder="Retype Password"><br>
+            <input type="submit" value="Sign Up"><br>
+        </form>
+    </div>
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
